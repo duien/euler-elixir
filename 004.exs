93f7e50e4feede7f1c9e3be98eea1f7f56dcefe7
Emily Price <price.emily@gmail.com>
2013-08-15 13:21:41 -0700

    Working solution to 4

---
+++ 004.exs
@@ -0,0 +1,36 @@
+# A palindromic number reads the same both ways. The largest palindrome made
+# from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+defmodule Euler do
+  def is_palindrome(n) do
+    integer_to_list(n) == Enum.reverse(integer_to_list(n))
+  end
+
+  def products_up_to(max, current_a // 1, current_b // 1, products // [])
+  def products_up_to(max, current_a, current_b, products)
+      when current_b == max and current_a == max do
+    products
+  end
+  def products_up_to(max, current_a, current_b, products)
+      when current_b == max do
+    products_up_to(max, current_a + 1, 1, [current_a * max] ++ products)
+  end
+  def products_up_to(max, current_a, current_b, products)
+      when current_a == max do
+    products_up_to(max, max, current_b + 1, [current_b * max] ++ products)
+  end
+  def products_up_to(max, current_a, current_b, products) do
+    products_up_to(max, current_a, current_b + 1, [current_a * current_b] ++ products)
+  end
+
+  def palindrome_products(max) do
+    Enum.filter( products_up_to(max), &is_palindrome/1 )
+  end
+
+  def largest_palindrome_product(max) do
+    Enum.max(palindrome_products(max))
+  end
+
+end
